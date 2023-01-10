eff05b94169c284d0e75370a1a34fd9c735e8c04
mittiede <sudakovkonst@yandex.ru>
2023-01-10 18:26:58 +0300

    task_change_implemented

--- toDoApp.py
+++ toDoApp.py
@@ -8,19 +8,31 @@ class ToDoApp(QMainWindow, Ui_main_window):
         super().__init__()
         self.setupUi(self)
         self.project = Project('today')
-        self.initial_state()
+        self.current_id = 1
+        self.show_task()
+
+        #connecting buttons
+        self.left_arrow.clicked.connect(self.previous_task)
+        self.right_arrow.clicked.connect(self.next_task)
     
-    def initial_state(self):
-        text = self.project.get_tasks_texts()[0][0]
-        title = self.project.get_tasks_titles()[0][0]
-        self.task_title.setText(title)
-        self.task_window.setText(text)
+    def show_task(self):
+        try:
+            text = self.project.get_tasks_texts()[self.current_id - 1][0]
+            title = self.project.get_tasks_titles()[self.current_id - 1][0]
+            self.task_title.setText(title)
+            self.task_window.setText(text)
+        except Exception:
+            print("error when trying to show task")
+            self.current_id = 1
+            self.show_task()
     
     def next_task(self):
-        pass
+        self.current_id += 1
+        self.show_task()
 
     def previous_task(self):
-        pass
+        self.current_id -= 1
+        self.show_task()
 
 
 app = QApplication()
